09264381712829920d8790da6696edfa43a26c09
Justine Gallien <justinegallien@gmail.com>
2025-07-23 20:09:30 -0500

    installed cors

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ const app = express();
 const port = 3000;
 import { employee } from "./routes/employeeRoute.js";
 import { task } from "./routes/taskRoute.js";
-
 app.use(express.json());
-
 app.use("/api/", employee);
 app.use("/api/", task);
-
 app.listen(port, () => {
   console.log(`listening in port ${port}`);
 });
