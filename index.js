a7e27d0a1273d29bd95677729647c905ae497e76
Justine Gallien <justinegallien@gmail.com>
2025-07-29 20:02:12 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 import express from "express";
+import fs from "fs";
+import https from "https";
 const app = express();
-const port = 3000;
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
 import { employee } from "./routes/employeeRoute.js";
 import { task } from "./routes/taskRoute.js";
+
+const enviroment = process.env.NODE_ENVIROMENT;
+let port = 3000;
+
+if (enviroment === "production") {
+  port = 443;
+}
 app.use(express.json());
+app.use(cors());
 app.use("/api/", employee);
 app.use("/api/", task);
-app.listen(port, () => {
-  console.log(`listening in port ${port}`);
-});
+
+if (enviroment === "production") {
+  const options = {
+    key: fs.readFileSync(
+      "/etc/letsencrypt/live/tasktracker-gallien.codex-p4-2025.click/privkey.pem"
+    ),
+    cert: fs.readFileSync(
+      "/etc/letsencrypt/live/tasktracker-gallien.codex-p4-2025.click/fullchain.pem"
+    ),
+  };
+
+  https.createServer(options, app).listen(443, () => {
+    console.log("HTTPS server is running on port 443");
+  });
+} else {
+  app.listen(port, () => {
+    console.log(`listening in port ${port}`);
+  });
+}
